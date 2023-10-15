27430995acedcda132ffa5c598140c95bcb1f9a9
Anton <110679298+Anton162@users.noreply.github.com>
2023-10-15 18:54:48 +0300

    added the application structur

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -3,18 +3,23 @@ import styled from "styled-components";
 import { Logo } from "../../components/logo/Logo";
 import { Menu } from "../../components/menu/Menu";
 import { Button } from "../../components/button/Button";
+import { FlexWrapper } from "../../components/FlexWrapper";
 
 export const Header = () => {
   return (
     <StyledHeader>
-      <Logo />
-      <Menu />
-      <Button />
+      <FlexWrapper align={"center"} justify={"space-between"}>
+        <Logo />
+        <Menu />
+        <Button />
+      </FlexWrapper>
     </StyledHeader>
   );
 };
 
 const StyledHeader = styled.header`
+  margin: 0 auto;
+  width: 100%;
   display: flex;
-  justify-content: space-between;
+  position: fixed;
 `;
